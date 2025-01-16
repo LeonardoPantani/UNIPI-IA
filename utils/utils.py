2a8f3808b1b1eb5d881e0392770264342bae5baa
Leonardo Pantani <leonardo.pantani@gmail.com>
2025-01-16 12:22:04 +0100

    supervised: added logistic regression, moved functions to utils utils: all functions definitions are here now

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import collections
-
+from collections import Counter
+from imblearn.under_sampling import RandomUnderSampler
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -8,17 +9,12 @@ from sklearn.model_selection import StratifiedKFold
 
 
 def printInfo(type, data):
-    # Crea un oggetto Counter a partire dall'array
     counter = collections.Counter(data)
-    # Calcola il numero di elementi per ogni intero
     counts = dict(counter)
-    # Calcola la percentuale rispetto al totale nell'array
     percentages = {k: v / len(data) for k, v in counts.items()}
-    # Stampa i risultati
-    print("Esempi di {} [tot: {}]:".format(type, len(data)))
+    print("{} samples [tot: {}]:".format(type, len(data)))
     for k,v in counts.items():
-        print(" Classe {}: {} ({:.2f}% del totale)".format(k, v, percentages[k]*100))
-
+        print(" {} class: {} ({:.2f}% of total)".format(k, v, percentages[k]*100))
 
 
 def evaluate_model_with_cv(x_train, y_train, classifier, x_test=None, y_test=None, target_names=['benign', 'phishing', 'defacement', 'malware'], k=5, need_results=False):
@@ -31,7 +27,7 @@ def evaluate_model_with_cv(x_train, y_train, classifier, x_test=None, y_test=Non
     - classifier: Model to be trained and evaluated.
     - x_test: (Optional) Features for testing.
     - y_test: (Optional) Labels for testing.
-    - target_names: (Optional) List containing list of classes' names, default ['benign', 'phishing', 'defacement', 'malware'] 
+    - target_names: (Optional) List containing list of classes' names, default ['benign', 'phishing', 'defacement', 'malware']
     - k: (Optional) Number of folds, default 5
     - need_results: (Optional) Set this to True if results as dictionary are needed.
 
@@ -61,7 +57,13 @@ def evaluate_model_with_cv(x_train, y_train, classifier, x_test=None, y_test=Non
         # Metrics
         cv_accuracy.append(accuracy_score(y_val_fold, y_val_pred))
         cv_f1.append(f1_score(y_val_fold, y_val_pred, average='weighted'))
-        cv_auc.append(roc_auc_score(y_val_fold, y_val_proba, multi_class='ovr'))
+        
+        # Adjust for binary case
+        if len(np.unique(y_train)) == 2:  # Binary classification
+            y_val_proba_positive = y_val_proba[:, 1]  # Select probabilities for positive class
+            cv_auc.append(roc_auc_score(y_val_fold, y_val_proba_positive))
+        else:  # Multiclass classification
+            cv_auc.append(roc_auc_score(y_val_fold, y_val_proba, multi_class='ovr'))
 
     # Cross-validation results
     cv_results = {
@@ -89,13 +91,18 @@ def evaluate_model_with_cv(x_train, y_train, classifier, x_test=None, y_test=Non
         print(report)
 
         # AUC score
-        auc_score = roc_auc_score(y_test, y_pred_proba, multi_class='ovr')
+        if len(np.unique(y_train)) == 2:  # Binary classification
+            y_pred_proba_positive = y_pred_proba[:, 1]
+            auc_score = roc_auc_score(y_test, y_pred_proba_positive)
+        else:  # Multiclass classification
+            auc_score = roc_auc_score(y_test, y_pred_proba, multi_class='ovr')
+
         print(f"\nAUC Score: {auc_score:.4f}")
 
         # Confusion Matrix
         conf_matrix = confusion_matrix(y_test, y_pred, labels=np.unique(y_test))
-        plt.figure(figsize=(6, 4))
-        sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=target_names, yticklabels=target_names)
+        plt.figure(figsize=(4, 3))
+        sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False, xticklabels=target_names, yticklabels=target_names)
         plt.xlabel('Predicted Class')
         plt.ylabel('True Class')
         plt.title('Confusion Matrix')
@@ -106,3 +113,43 @@ def evaluate_model_with_cv(x_train, y_train, classifier, x_test=None, y_test=Non
         cv_results["test_confusion_matrix"] = conf_matrix
 
     return cv_results if need_results else None
+
+
+def balance_data_undersample_benign(x_train, y_train):
+    class_counts = Counter(y_train)
+    other_classes_count = sum([class_counts[label] for label in class_counts if label != "benign"])
+    benign_target = other_classes_count
+    undersampling_strategy = {"benign": benign_target}
+    rus = RandomUnderSampler(sampling_strategy=undersampling_strategy, random_state=42)
+    x_train_resampled, y_train_resampled = rus.fit_resample(x_train, y_train)
+    return x_train_resampled, y_train_resampled
+
+
+def remove_outliers(df, features_to_clean=['url_entropy', 'num_digits', 'dot_number', 'directory_num', 'hostname_length', 'top_level_domain_length', 'num_letters']):
+    """
+    This function cleans the dataset from outliers.
+
+    Parameters:
+    - df: The dataframe from which you want the outliers to be removed.
+
+    Returns:
+    - A dataframe cleaned from outliers
+    """
+
+    df_clean = df.copy()
+    for column in features_to_clean:
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 3 * IQR
+        upper_bound = Q3 + 3 * IQR
+        df_clean = df_clean[
+            (df_clean[column] >= lower_bound) & 
+            (df_clean[column] <= upper_bound)
+        ]
+    return df_clean
+
+
+def merge_classes_to_binary(y):
+    y = y.cat.add_categories("malignant")
+    return y.replace({"malware": "malignant", "defacement": "malignant", "phishing": "malignant"}).cat.remove_unused_categories()
